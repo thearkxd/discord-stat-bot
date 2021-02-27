9d00eec1c299b6d98b41024f541799b5cbbb689d
Theark <59927799+thearkxd@users.noreply.github.com>
2021-02-28 01:01:06 +0300

    Kanal değiştirince veri kaydetmeme sorunu düzeltildi.

--- src/events/voiceStats.js
+++ src/events/voiceStats.js
@@ -7,28 +7,35 @@ const userParent = require("../schemas/voiceUserParent");
 
 module.exports = async (oldState, newState) => {
   if ((oldState.member && oldState.member.user.bot) || (newState.member && newState.member.user.bot)) return;
-
-  let user;
-  if (oldState.channelID && !newState.channelID) user = oldState;
-  else if ((oldState.channelID && newState.channelID) || (!oldState.channelID && newState.channelID)) user = newState;
-
-  if ((oldState.channelID && newState.channelID) || (!oldState.channelID && newState.channelID)) {
-    await joinedAt.findOneAndUpdate({ userID: user.id }, { $set: { date: Date.now() } }, { upsert: true });
+  
+  if (!oldState.channelID && newState.channelID) {
+    await joinedAt.findOneAndUpdate({ userID: newState.id }, { $set: { date: Date.now() } }, { upsert: true });
   }
-  const joinedAtData = await joinedAt.findOne({ userID: user.id });
+
+  const joinedAtData = await joinedAt.findOne({ userID: oldState.id });
+
+  if (!joinedAtData) {
+    await joinedAt.findOneAndUpdate({ userID: oldState.id }, { $set: { date: Date.now() } }, { upsert: true });
+  }
+
   const data = Date.now() - joinedAtData.date;
 
   if (oldState.channelID && !newState.channelID) {
-    if (!joinedAtData) return;
+    await saveDatas(oldState, oldState.channel, data);
     await joinedAt.deleteOne({ userID: oldState.id });
+  } else if (oldState.channelID && newState.channelID) {
+    await saveDatas(oldState, oldState.channel, data);
+    await joinedAt.findOneAndUpdate({ userID: oldState.id }, { $set: { date: Date.now() } }, { upsert: true });
   }
+};
 
+async function saveDatas(user, channel, data) {
   await voiceUser.findOneAndUpdate({ guildID: user.guild.id, userID: user.id }, { $inc: { topStat: data, dailyStat: data, weeklyStat: data, twoWeeklyStat: data } }, { upsert: true });
   await voiceGuild.findOneAndUpdate({ guildID: user.guild.id }, { $inc: { topStat: data, dailyStat: data, weeklyStat: data, twoWeeklyStat: data } }, { upsert: true });
-  await guildChannel.findOneAndUpdate({ guildID: user.guild.id, channelID: user.channelID }, { $inc: { channelData: data } }, { upsert: true });
-  await userChannel.findOneAndUpdate({ guildID: user.guild.id, userID: user.id, channelID: user.channelID }, { $inc: { channelData: data } }, { upsert: true });
-  if (user.channel.parent) await userParent.findOneAndUpdate({ guildID: user.guild.id, userID: user.id, parentID: user.channel.parentID }, { $inc: { parentData: data } }, { upsert: true });
-};
+  await guildChannel.findOneAndUpdate({ guildID: user.guild.id, channelID: channel.id }, { $inc: { channelData: data } }, { upsert: true });
+  await userChannel.findOneAndUpdate({ guildID: user.guild.id, userID: user.id, channelID: channel.id }, { $inc: { channelData: data } }, { upsert: true });
+  if (channel.parent) await userParent.findOneAndUpdate({ guildID: user.guild.id, userID: user.id, parentID: channel.parentID }, { $inc: { parentData: data } }, { upsert: true });
+}
 
 module.exports.conf = {
   name: "voiceStateUpdate",
